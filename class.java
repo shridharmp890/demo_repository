3108a365e4c72b9d788be63c2b7c17f60c5d589d
shridharmp890 <shridharmp3@gmail.com>
2026-03-14 20:12:38 +0530

    Add files via upload

---
+++ class.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 25;
+        int sum = a = b;
+        System.out.println(sum);
+    }
+}
---
+++ sapna.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class sapna {
+    public static void main(String[] args){
+        Scanner sc = new scanner(System.in);
+        String name sc.next();
+        System.out.println(name);
+
+        }
+    }
+}
